11cb107910951c655ded98d7aa17a177fb05dd3f
jakubjakubowski8 <jakubjakubowski8@gmail.com>
2021-03-25 14:34:47 +0100

    Create simple calculator

--- src/main/java/jj/StringCalculator.java
+++ src/main/java/jj/StringCalculator.java
@@ -1,4 +1,26 @@
 package jj;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+  private final String REGEX = ",";
+
+
+  public int add(String input) {
+
+    if (input == null || input.isEmpty()) {
+      return 0;
+    }
+
+    return sumString(input);
+  }
+
+  private int sumString(String input) {
+
+    return Pattern.compile(REGEX)
+        .splitAsStream(input)
+        .mapToInt(Integer::parseInt).sum();
+
+  }
 }
--- src/test/java/jj/StringCalculatorTest.java
+++ src/test/java/jj/StringCalculatorTest.java
@@ -1,4 +1,67 @@
 package jj;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+
 public class StringCalculatorTest {
+
+  private StringCalculator stringCalculator = new StringCalculator();
+
+  @Test
+  public void shouldReturn0ForEmptyInput() {
+
+    String input = "";
+    int expectedResult = 0;
+
+    int result = stringCalculator.add(input);
+
+    assertThat(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  public void shouldReturn1ForInput1() {
+
+    String input = "1";
+    int expectedResult = 1;
+
+    int result = stringCalculator.add(input);
+
+    assertThat(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  public void shouldReturn2ForInput2() {
+
+    String input = "2";
+    int expectedResult = 2;
+
+    int result = stringCalculator.add(input);
+
+    assertThat(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  public void shouldReturn3ForInput1Coma2() {
+
+    String input = "1,2";
+    int expectedResult = 3;
+
+    int result = stringCalculator.add(input);
+
+    assertThat(result).isEqualTo(expectedResult);
+  }
+
+  @Test
+  public void shouldReturn7ForLongComaSeparatedString() {
+
+    String input = "1,2,2,1,1";
+    int expectedResult = 7;
+
+    int result = stringCalculator.add(input);
+
+    assertThat(result).isEqualTo(expectedResult);
+  }
 }
+
